843b12e603d52fd192841f904634d49e0a95c6f8
SandipanBhattacharjee <sandipanbhattacharjee1981@gmail.com>
2020-05-26 16:13:00 -0500

    making changes to classone

--- restassured/src/test/java/testngfeatures/ClassOne.java
+++ restassured/src/test/java/testngfeatures/ClassOne.java
@@ -10,19 +10,21 @@ public class ClassOne {
 	public void methodOne() {
 //		int a = 10, b=20;
 //		Assert.assertEquals(a, b);	
+		System.out.println(System.getProperty("property1")+" "+System.getProperty("property2"));
 		System.out.println("This is method one");
 			
 	}
 	
 	@Test(priority = 2)
 	public void methodTwo() {
-		int c =10, d =20;
+		int c =20, d =20;
 		SoftAssert soft = new SoftAssert();
 		soft.assertEquals(c, d);		
 		System.out.println("This is method two");
 		System.out.println("This is before the soft assertions");
 		soft.assertAll("Check for failures");
 		System.out.println("This is after the soft assertions");
+		System.out.println(System.getProperty("property1")+" "+System.getProperty("property2"));
 	}
 	
 	@Test(priority = 3, groups = {"smoke"})
